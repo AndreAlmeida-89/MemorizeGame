b5dfcfcda2439258db6de6e58f4faf6958e32892
AndreAlmeida-89 <andre_rdo@hotmail.com>
2021-06-13 11:41:54 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Andre Almeida on 13/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
